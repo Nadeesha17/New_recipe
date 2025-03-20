b7178f786d6652e562644f26d9fb25c074f56719
Nadeesha Illamperuma <nadisliit@gmail.com>
2025-03-21 00:44:10 +0100

    ss

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,8 +7,21 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    recipe_id = params[:id]
+
+    api_key = ENV["SPOONACULAR_API_KEY"]
+    url = "https://api.spoonacular.com/recipes/#{recipe_id}/information?apiKey=#{api_key}"
+    response = HTTParty.get(url)
+
+    if response.success?
+      @recipe = response.parsed_response
+    else
+      flash[:alert] = "Error fetching recipe details: #{response.message} (Code: #{response.code})"
+      @recipe = {}
+    end
   end
+
+
 
 
     def ingredient
@@ -36,7 +49,28 @@ class RecipesController < ApplicationController
           @recipes = []
         end
       end
+      def type
+        meal_type = params[:type]
+
+        if meal_type.present?
+          api_key = ENV["SPOONACULAR_API_KEY"]
+          url = "https://api.spoonacular.com/recipes/complexSearch?type=#{meal_type}&apiKey=#{api_key}"
+          response = HTTParty.get(url)
+
+          if response.success? && response.parsed_response["results"].present?
+            @recipes = response.parsed_response["results"]
+          else
+            flash[:alert] = "No recipes found for meal type: #{meal_type} or an error occurred."
+            @recipes = []
+          end
+        else
+          @recipes = []
+        end
+      end
+
     end
+
+
 
 
     private
@@ -65,70 +99,3 @@ class RecipesController < ApplicationController
         @recipes = []
       end
     end
-
-
-  class RecipesController < ApplicationController
-    require 'httparty'
-
-    def index
-    end
-
-    def show
-      @recipe = Recipe.find(params[:id])
-    end
-
-    # Ingredient-based recipe search
-    def ingredient
-      if params[:ingredients].present?
-        fetch_recipes
-      else
-        @recipes = []
-      end
-    end
-
-
-    def type
-      meal_type = params[:type]
-
-      if meal_type.present?
-        api_key = ENV["SPOONACULAR_API_KEY"]
-        url = "https://api.spoonacular.com/recipes/complexSearch?type=#{meal_type}&apiKey=#{api_key}"
-        response = HTTParty.get(url)
-
-        Rails.logger.info "API Request URL: #{url}"
-        Rails.logger.info "API Response: #{response.body}"
-
-        if response.success? && response.parsed_response["results"].present?
-          @recipes = response.parsed_response["results"]
-        else
-          flash[:alert] = "No recipes found for meal type: #{meal_type} or an error occurred."
-          @recipes = []
-        end
-      else
-        @recipes = []
-      end
-    end
-
-    # Dietary-based recipe search
-    def dietary
-      dietary_type = params[:dietary]
-
-      if dietary_type.present?
-        api_key = ENV["SPOONACULAR_API_KEY"]
-        url = "https://api.spoonacular.com/recipes/complexSearch?diet=#{dietary_type}&apiKey=#{api_key}"
-        response = HTTParty.get(url)
-
-        Rails.logger.info "API Request URL: #{url}"
-        Rails.logger.info "API Response: #{response.body}"
-
-        if response.success? && response.parsed_response["results"].present?
-          @recipes = response.parsed_response["results"]
-        else
-          flash[:alert] = "No recipes found for #{dietary_type} or an error occurred."
-          @recipes = []
-        end
-      else
-        @recipes = []
-      end
-    end
-  end
