e369bf734e4ecf6a54ce93069c870d281e5fba03
Nadeesha Illamperuma <nadisliit@gmail.com>
2025-03-25 15:43:07 +0100

    frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_23_102013) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_22_060825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_23_102013) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
+    t.text "ingredients"
     t.text "instructions"
     t.string "meal_type"
     t.string "cuisine"
@@ -79,7 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_23_102013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.text "ingredients"
   end
 
   create_table "users", force: :cascade do |t|
