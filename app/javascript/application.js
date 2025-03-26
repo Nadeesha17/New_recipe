c46ecf1062f374482b5f2a4345a0e4373bc571b0
Nadeesha Illamperuma <nadisliit@gmail.com>
2025-03-26 20:26:50 +0100

    Resolve merge conflict in _fonts.scss

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,20 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 768) {
+    console.log("Mobile view");
+  } else {
+    console.log("Desktop view");
+  }
+});
+
+// Optional: Run on initial load to detect the screen size immediately
+if (window.innerWidth < 768) {
+  console.log("Mobile view");
+} else {
+  console.log("Desktop view");
+}
